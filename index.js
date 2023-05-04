a1ba12e7a73639ea15b9094b2330fb9fcb1865c5
Margub Murshed <murshedmargub@gmail.com>
2023-05-04 22:51:22 +0600

    Made necessary endpoints

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 const express = require("express");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
+const chefs = require('./data/chefs.json');
+const recipes = require('./data/recipes.json');
+
 const app = express();
 app.use(cors());
+
+app.get('/allChefsDetails', (req, res) => {
+    res.send(chefs);
+})
+
+app.get('/chef/:id/details', (req, res) => {
+    const id = req.params.id;
+    const selectedChef = chefs.chefs.find(chef => chef.id === id);
+    res.send(selectedChef)
+})
+
+app.get('/chef/:id/recipes', (req, res) => {
+    const chefID = req.params.id;
+    const chefRecipes = recipes[chefID]
+    res.send(chefRecipes)
+})
 
 app.listen(port, () => console.log('listening port' + port))
