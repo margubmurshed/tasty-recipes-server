272e5569578391e8743699b46ebb8018b1e2825b
Margub Murshed <murshedmargub@gmail.com>
2023-05-04 22:49:21 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+const app = express();
+app.use(cors());
+
+app.listen(port, () => console.log('listening port' + port))
